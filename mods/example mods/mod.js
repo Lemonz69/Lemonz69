a9a5a652f109f65a6c436b335949595e45e620a1
Lemonz69 <136924376+Lemonz69@users.noreply.github.com>
2024-06-03 17:15:23 +0200

    Update mod.js

--- mods/example mods/mod.js
+++ mods/example mods/mod.js
@@ -1,1 +1,6 @@
-
+// mods/example-mod/mod.js
+document.addEventListener('DOMContentLoaded', () => {
+    const newElement = document.createElement('p');
+    newElement.textContent = 'This is an example mod!';
+    document.body.appendChild(newElement);
+});
